9794ade03bbf9652ae18b92255d76b60e3671a8b
fccf <xiyanfc@qq.com>
2018-03-18 20:31:54 +0800

    update learn time

--- src/learn_time.f90
+++ src/learn_time.f90
@@ -47,6 +47,31 @@ contains
 
   end function s2hms
 
+
+  function now()
+    !< return time now as a string
+    character(:), allocatable :: now
+
+    integer :: y,m,d,h,n,s,values(8)
+    character(len=10) :: date
+    character(len=8)  :: time
+
+    call date_and_time ( values = values )
+
+    y = values(1)
+    m = values(2)
+    d = values(3)
+    h = values(5)
+    n = values(6)
+    s = values(7)
+
+    write(date,'(i0,a,i0,a,i0)') y,'-',m,'-',d
+    write(time,'(i0,a,i0,a,i0)') h,':',n,':',s
+
+    now = trim(adjustl(date))//' '//trim(adjustl(time))
+
+  end function now
+
 end module time
 
 program main
@@ -58,6 +83,7 @@ program main
   call sleep(1)
   dt = wall_time() - t
 
+  print*, now()
   print*, s2hms(dt)
 
 end program main
